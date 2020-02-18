c1a7b345efaa8ea4c5e531e22a5d1193de869d41
Karmar Barrett <karmar.barrett97@gmail.com>
2020-02-18 20:11:15 +1100

    chore: update node packages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Under Maintenece</h1>
+    <p>
+      Sorry it appears my site is under construction at the moment, try again
+      late to witness the greatness{" "}
+    </p>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
